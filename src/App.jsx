37eb6ca3b35a87096e94760af4fdfcdc25949ed9
DevEwertonOliveira <ewerton2007@gmail.com>
2023-11-02 16:38:12 -0400

    basic configuration css and react

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-function App() {
+import { Post } from "./Post"
+import { Header } from "./components/Header"
+
+import "./global.css"
+
+export function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div>
+      <Header />
+      <Post
+        author="Ewerton Oliveira"
+        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, animi. Quo deserunt sequi corporis et esse quae ipsa tenetur, a officia impedit, omnis tempora ducimus magni quasi laborum quisquam dolor."
+      />
+      <Post
+        author="Gabriel Buzzi"
+        content="Um novo post muito legal"
+      />
+
+    </div>
   )
 }
-export default App
+
