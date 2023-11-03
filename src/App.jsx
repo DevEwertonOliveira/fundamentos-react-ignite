9fa561b4748dee7249b13e30d9d086001a6d2f6c
DevEwertonOliveira <ewerton2007@gmail.com>
2023-11-03 17:51:24 -0400

    sidebar finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,27 @@ import { Post } from "./Post"
 import { Header } from "./components/Header"
 
 import "./global.css"
+import styles from "./App.module.css"
+import { Sidebar } from "./components/Sidebar"
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Ewerton Oliveira"
-        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, animi. Quo deserunt sequi corporis et esse quae ipsa tenetur, a officia impedit, omnis tempora ducimus magni quasi laborum quisquam dolor."
-      />
-      <Post
-        author="Gabriel Buzzi"
-        content="Um novo post muito legal"
-      />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Ewerton Oliveira"
+            content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, animi. Quo deserunt sequi corporis et esse quae ipsa tenetur, a officia impedit, omnis tempora ducimus magni quasi laborum quisquam dolor."
+          />
+          <Post
+            author="Gabriel Buzzi"
+            content="Um novo post muito legal"
+          />
+        </main>
+      </div>
 
     </div>
   )
