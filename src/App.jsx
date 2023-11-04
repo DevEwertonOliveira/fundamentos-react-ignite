7b6806608afaae0944414b14cb9ae40c3b8a5c87
DevEwertonOliveira <ewerton2007@gmail.com>
2023-11-04 09:11:43 -0400

    post e css prontos

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { Post } from "./Post"
 import { Header } from "./components/Header"
-
-import "./global.css"
-import styles from "./App.module.css"
+import { Post } from "./components/Post"
 import { Sidebar } from "./components/Sidebar"
+
+import styles from "./App.module.css"
+import "./global.css"
 
 export function App() {
   return (
@@ -13,14 +13,8 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Ewerton Oliveira"
-            content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam, animi. Quo deserunt sequi corporis et esse quae ipsa tenetur, a officia impedit, omnis tempora ducimus magni quasi laborum quisquam dolor."
-          />
-          <Post
-            author="Gabriel Buzzi"
-            content="Um novo post muito legal"
-          />
+          <Post />
+          <Post />
         </main>
       </div>
 
