b150c94d1999f79c68b553b26f6188e6379b03e1
DevEwertonOliveira <ewerton2007@gmail.com>
2023-11-11 15:30:43 -0400

    ajustes componetização, map e data

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,36 @@ import { Sidebar } from "./components/Sidebar"
 import styles from "./App.module.css"
 import "./global.css"
 
-
-
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/DevEwertonOliveira.png",
+      name: "Ewerton Oliveira",
+      role: "Web Developer"
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀 ' },
+      { type: 'link', content: '<a href="#">jane.design/doctorcare</a> ' },
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/maykbrito.png",
+      name: "Mayk Brito",
+      role: "Web Developer"
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀 ' },
+      { type: 'link', content: '<a href="#">jane.design/doctorcare</a> ' },
+    ],
+    publishedAt: new Date('2022-05-10 20:00:00')
+  },
+]
 
 export function App() {
   return (
@@ -16,8 +44,13 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+          {posts.map((post, index) => {
+            return (<Post key={index}
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />)
+          })}
         </main>
       </div>
 
